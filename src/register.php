6d840fd8eb36f39dad3ec4a8d2774035204a4d15
ziborge <fayrlysson2@gmail.com>
2025-11-12 14:39:43 -0300

    Update register.php

--- src/register.php
+++ src/register.php
@@ -5,13 +5,14 @@ if (isset($_POST["submit"])) {
     $confirmarEmail = htmlspecialchars($_POST["confirmarEmail"]);
     $senha = $_POST["senha"];
     $confirmarSenha = $_POST["confirmarSenha"];
+}
     // 3. Validação do email
     $erros = [];
     if (empty($_POST['email'])) {
         $erros[] = "preecha o campo email.";
     }
-    elseif (!filter_var($_POST['email'], filter_validate_email)) {
-        $erros[] = "email Invalido"
+    elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $erros[] = "email Invalido";
     }
     // 4. Vaidação da senha
     $senha = $_POST['senha'];
@@ -19,4 +20,19 @@ if (isset($_POST["submit"])) {
     if ($senha !== $confirmarSenha) {
         $erros[] = "As senhas não são iguais.";
     }
-    // Estudar senha em hash & Armazenar Os Dados .
+    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
+    // 5. Conexão com o Banco de Dados
+     $host = "localhost";
+     $database = "Projeto_Trabalho";
+     $user = "alex"; 
+     $pass = "Pato";
+     try {
+    
+        $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $pdo->prepare("INSERT INTO usuarios (email, senha) VALUES (:email, :senha)"); 
+    $stmt->execute(['$email', $senhaHash]);
+    echo "<h1>Cadastro Realizado!</h1>";
+    exit();
+     }
+    ?>
